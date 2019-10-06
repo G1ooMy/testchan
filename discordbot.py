5b365e0dfef10eca4b7e4444ff4c8335467e5e2b
discord.py <56204626+G1ooMy@users.noreply.github.com>
2019-10-06 10:54:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,3 +27,11 @@ async def on_message(message):
         # [!dice ]部分を消し、AdBのdで区切ってリスト化する
         order = say.strip('!dice ')
         cnt, mx = list(map(int, order.split('d'))) # さいころの個数と面数
+        dice = diceroll(cnt, mx) # 和を計算する関数(後述)
+        await message.channel.send(dice[cnt])
+        del dice[cnt]
+
+        # さいころの目の総和の内訳を表示する
+        await message.channel.send(dice)
+
+client.run(token)
